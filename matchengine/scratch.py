7571170519f8f43087c04d4c296699677e738160
Sumedha <sumedha.saxena@gmail.com>
2025-03-03 18:24:57 +0800

    Retrieve latest oncotree data(oncotree_2021_11_02) and updated the script for converting the downloaded data into oncotree_mapping.json

--- matchengine/scratch.py
+++ matchengine/scratch.py
@@ -4,24 +4,28 @@ import csv
 import os
 from collections import defaultdict
 
-ONCOTREE_TXT_FILE_PATH = os.getenv("ONCOTREE_TXT_FILE_PATH", None)
-
-with open(
-        ONCOTREE_TXT_FILE_PATH) as f:
+ONCOTREE_TXT_FILE_PATH = "C:\HKU\Matchminer\oncotree_file.txt"
+#os.getenv("ONCOTREE_TXT_FILE_PATH", None)
+#console.log(ONCOTREE_TXT_FILE_PATH)
+with open(ONCOTREE_TXT_FILE_PATH) as f:
     r = csv.DictReader(f, delimiter='\t')
     rows = [row for row in r]
 mapping = defaultdict(set)
 for row in rows:
-    primary = row['primary'].split('(')[0].strip()
-    secondary = row['secondary'].split('(')[0].strip()
-    tertiary = row['tertiary'].split('(')[0].strip()
-    quaternary = row['quaternary'].split('(')[0].strip()
-    quinternary = row['quinternary'].split('(')[0].strip()
-    mapping[primary].update({primary, secondary, tertiary, quaternary, quinternary})
-    mapping[secondary].update({secondary, tertiary, quaternary, quinternary})
-    mapping[tertiary].update({tertiary, quaternary, quinternary})
-    mapping[quaternary].update({quaternary, quinternary})
-    mapping[quinternary].update({quinternary})
+    level_1 = row['level_1'].split('(')[0].strip()
+    level_2 = row['level_2'].split('(')[0].strip()
+    level_3 = row['level_3'].split('(')[0].strip()
+    level_4 = row['level_4'].split('(')[0].strip()
+    level_5 = row['level_5'].split('(')[0].strip()
+    level_6 = row['level_6'].split('(')[0].strip()
+    level_7 = row['level_7'].split('(')[0].strip()
+    mapping[level_1].update({level_1, level_2, level_3, level_4, level_5, level_6, level_7})
+    mapping[level_2].update({level_2, level_3, level_4, level_5, level_6, level_7})
+    mapping[level_3].update({level_3, level_4, level_5, level_6, level_7})
+    mapping[level_4].update({level_4, level_5, level_6, level_7})
+    mapping[level_5].update({level_5, level_6, level_7})
+    mapping[level_6].update({level_6, level_7})
+    mapping[level_7].update({level_7})
 
 del mapping['']
 for s in mapping.values():
@@ -32,7 +36,7 @@ for k in list(mapping.keys()):
     if k not in mapping['_LIQUID_']:
         mapping['_SOLID_'].add(k)
 
-for k in mapping.keys():
+for k in mapping.keys():#
     mapping[k] = list(mapping[k])
 
 import json
